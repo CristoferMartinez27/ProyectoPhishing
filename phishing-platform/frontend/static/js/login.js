d30cb7ff72aa39218ca5479b13d9b21454600706
CristoferMartinez27 <cmartinezm14@miumg.edu.gt>
2025-11-07 02:23:56 -0600

    Actualizar URL del backend en frontend

--- phishing-platform/frontend/static/js/login.js
+++ phishing-platform/frontend/static/js/login.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://proyectophishing-production.up.railway.app/';
 
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
     e.preventDefault();
